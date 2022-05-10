cf6ed81a151f344cfe91374c91b71b4186b463bb
tribuanaayu <103018051+tribuanaayu@users.noreply.github.com>
2022-05-10 15:48:54 +0700

    Selesai

--- resources/views/prodi/index.blade.php
+++ resources/views/prodi/index.blade.php
@@ -4,14 +4,46 @@
 @section("title", "Halaman Program Studi")
 
 @section("content")
-    <h1>Program Studi</h1>
-    @if(count($prodi) > 0 )
-        @foreach($prodi as $item)
-            <li>{{ $item->nama }} {{ $item->fakultas->nama }}</li>
-        @endforeach
-    @else
-        Program Studi tidak ditemukan
-    @endif
+    <div class="row pt-4">
+        <div class="col">            
+            <h1>Program Studi</h1>
+            <div class="d-md-flex justify-content-md-end">
+                <a href="{{ route('prodi.create') }}" class="btn btn-primary">Tambah</a>
+            </div>
+            @if (session()->has('info'))
+                <div class="alert alert-success">
+                    {{ session()->get('info')}}
+                </div>
+            @endif
+            <div>
+                <table class="table table-striped table-hover">
+                    <thread>
+                        <tr>
+                            <th>Nama</th>
+                            <th>Fakultas</a>
+                            <th>Aksi</th>
+                        </tr>
+                    </thread>
+                    <tbody>
+                    @foreach($prodi as $item)
+                        <tr>
+                            <td> {{ $item->nama }} </td>
+                            <td> {{ $item->fakultas->nama }} </td>
+                            <td>
+                                <form action="{{ route('prodi.destroy', ['prodi' => $item->id]) }}" method="POST">
+                                    <a href="{{ url('/prodi/'.$item->id) }}" class="btn btn-warning">Detail</a>
+                                    <a href="{{ url('/prodi/'.$item->id.'/edit') }}" class="btn btn-info">Ubah</a>
+                                    @method('DELETE')
+                                    @csrf
+                                    <button type="submit" class="btn btn-danger">Hapus</button>
+                                </form>
+                            </td>
+                        </tr>
+                        {{-- <li>{{ $item->nama }} {{ $item->fakultas->nama }}</li> --}}
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 @endsection
-
-{{-- @include("layout.footer") --}}
--- routes/web.php
+++ routes/web.php
@@ -50,5 +50,10 @@ Route::get("/prodi/detail/{id?}", [ProdiController::class, "detail"]);
 Route::get("/mahasiswa", [MahasiswaController::class, "index"]);
 Route::get("/mahasiswa/detail/{id}", [MahasiswaController::class, "detail"])->name('detailmhs');
 
-Route::get('/prodi/create', [ProdiController::class,'create']);
-Route::post('prodi/store',[ProdiController::class,'store']);
+Route::get("/prodi/create", [ProdiController::class, "create"])->name("prodi.create");
+Route::post('prodi/store', [ProdiController::class, "store"])->name("prodi.store");
+Route::get('/prodi', [ProdiController::class, 'index'])->name('prodi.index');
+Route::get('/prodi/{prodi}', [ProdiController::class, 'show'])->name('prodi.show');
+Route::get('/prodi/{prodi}/edit', [ProdiController::class, 'edit'])->name('prodi.edit');
+Route::patch('/prodi/{prodi}', [ProdiController::class, 'update'])->name('prodi.update');
+Route::delete('/prodi/{prodi}', [ProdiController::class, 'destroy'])->name('prodi.destroy');
