797b80499d275d9849059e5ec03a237aefcebbcc
tribuanaayu <103018051+tribuanaayu@users.noreply.github.com>
2022-05-10 15:28:55 +0700

    Controller

--- app/Http/Controllers/ProdiController.php
+++ app/Http/Controllers/ProdiController.php
@@ -9,38 +9,20 @@ use Illuminate\Support\Facades\DB;
 class ProdiController extends Controller
 {
 
-    //function index(){
-        /*$data = [
-            'prodi' => ['Manajemen Informatika', 'Sistem Informasi', 'Informatika']
-        ];
+    function index(){
     
-        //atau menggunakan compact
-        $prodi = ['Manajemen Informatika', 'Sistem Informasi', 'Informatika'];
         $kampus = "Universitas Multi Data Palembang";
-    
-        return view("prodi.index", compact('prodi', 'kampus'));*/
-
-       //$kampus = "Universitas Multi Data Palembang";
-       // $prodi = Prodi::all();
-
-        /*$prodi = DB::select("SELECT prodi.*, fakultas.nama as namaf FROM prodi INNER JOIN fakultas ON prodi.fakultas_id = fakultas.id");*/
-        
-        //return view("prodi.index", compact('kampus', 'prodi'));
-//}
+        $prodi = Prodi::all();
+        return view('prodi.index')->with('prodi', $prodi);
+    }
 
     function detail($id = null){
         echo $id;
     }
-}
-    public function create()
-{
-    return view ("prodi.create");
-}
-public function store(Request $request)
-{
-    //dump($request)
-    //echo $request->nama;
-    $validateData = $request->validate(['nama'=> 'required|min:5|max:20',]);
-    dump($validateData);
-    echo $validateData['nama'];
+
+    function create(){
+        return view("prodi.create");
+    }
+
+    
 }
