484b4629cd6a54e09d73ff5ab53bc665130a5583
tribuanaayu <103018051+tribuanaayu@users.noreply.github.com>
2022-05-10 12:30:15 +0700

    validasi form

--- resources/views/prodi/create.blade.php
+++ resources/views/prodi/create.blade.php
@@ -10,3 +10,14 @@
     
 </body>
 </html>
+<h2>Form Prodi </h2>
+<form action= "{{url('prodi/store')}}"methode="post">
+    @call_user_func<div class="form-group">
+        <label for="nama">Nama</label>
+        <input type= "text" name="nama" id="nama" class="form-control" value="{{old('nama')}}">
+        @error('nama')
+        <div class="text-danger">{{$message}} </div>
+        @enderror
+</div>
+<button type="submit" class="btn-primary mt-2">Simpan</button>
+</form>
