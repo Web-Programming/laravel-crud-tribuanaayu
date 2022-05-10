37e09a63e87660fbd9c8c51aa4a56ce17897bf1e
tribuanaayu <103018051+tribuanaayu@users.noreply.github.com>
2022-05-10 15:29:31 +0700

    Controller lanjutan

--- app/Http/Controllers/ProdiController.php
+++ app/Http/Controllers/ProdiController.php
@@ -23,6 +23,49 @@ class ProdiController extends Controller
     function create(){
         return view("prodi.create");
     }
+    public function store(Request $request)
+    {
+        $validateData = $request->validate([
+            'nama' => 'required|min:5|max:20',
+        ]);
+       
+        $prodi = new Prodi(); //buat object prodi
+        $prodi->nama = $validateData['nama']; //simpan nilai inout ($validateData['nama]) ke dalam property nama prodi ($prodi->nama)
+        $prodi->institusi_id = 0;
+        $prodi->fakultas_id = 1;
+        $prodi->save(); //simpan ke dalam tabel prodis
 
+        //return "Data prodi $prodi->nama berhasil disimpan ke database"; // tampilkan pesan berhasil
+        $request->session()->flash('info', "Data prodi $prodi->nama berhasil disimpan ke database");
+        return redirect()->route('prodi.create');
+    }
+
+    public function show(Prodi $prodi)
+    {
+        return view('prodi.show', ['prodi' => $prodi]);
+    }
+
+    public function edit(Prodi $prodi)
+    {
+        return view('prodi.edit', ['prodi' => $prodi]);
+    }
+
+    public function update(Request $request, Prodi $prodi)
+    {
+     
+        $validateData = $request->validate([
+            'nama' => 'required|min:5|max:20',
+        ]);
+
+        Prodi::where('id', $prodi->id)->update($validateData);
+        $request->session()->flash('info', "Data prodi $prodi->nama berhasil diubah");
+        return redirect()->route('prodi.index');
+    }
+
+    public function destroy(Prodi $prodi)
+    {
+        $prodi->delete();
+        return redirect()->route('prodi.index')->with("info", "Prodi $prodi->nama berhasil dihapus.");
+    }
     
 }
