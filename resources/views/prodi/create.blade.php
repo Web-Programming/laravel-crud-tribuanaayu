67dd019cd7a7534cacfe6d737410aa6ed711d926
tribuanaayu <103018051+tribuanaayu@users.noreply.github.com>
2022-05-10 15:32:33 +0700

    create blade

--- resources/views/prodi/create.blade.php
+++ resources/views/prodi/create.blade.php
@@ -4,20 +4,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Form Prodi</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-    
+    <div class="container">
+        <div class="row pt-4">
+            <div class="col">
+                <h2>Form Prodi</h2>
+                <form action="{{ url('prodi/store') }}" method="post">
+                    @csrf   
+                    <div class="form-group">
+                        <label for="nama">Nama</label>
+                        <input type="text" name="nama" id="nama" class="form-control">
+                    </div>
+                    <button type="submit" class="btn btn-primary mt-2">Simpan</button>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
-<h2>Form Prodi </h2>
-<form action= "{{url('prodi/store')}}"methode="post">
-    @call_user_func<div class="form-group">
-        <label for="nama">Nama</label>
-        <input type= "text" name="nama" id="nama" class="form-control" value="{{old('nama')}}">
-        @error('nama')
-        <div class="text-danger">{{$message}} </div>
-        @enderror
-</div>
-<button type="submit" class="btn-primary mt-2">Simpan</button>
-</form>
